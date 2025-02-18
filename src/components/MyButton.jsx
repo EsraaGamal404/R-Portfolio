408e85020d39c7b6570289ae16b82dd150691f28
Esraa gamal <eg6237561@gmail.com>
2025-02-18 22:48:22 +0200

    Update MyButton.jsx

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,7 +1,5 @@
 
 function MyButton(){
- 
-  }
   return <button onClick={handleclick} className="font-bold text-xl bg-white p-2 pl-4 pr-4 mt-5  rounded	border-b-2 border-t-2 border-r-2 border-l-2  border-black	hover:bg-zinc-800 hover:text-white hover:ease-in duration-150">Hire me {count} </button>
 }
 export default function MyApp(){
