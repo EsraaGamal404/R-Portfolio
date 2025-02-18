49054484fc8e5cf172254ce75ce69b3d1a66405b
Esraa gamal <eg6237561@gmail.com>
2025-02-18 22:39:31 +0200

    Update MyButton.jsx

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 
 function MyButton(){
-  const [count,setcount] = useState();
-  function handleclick(){
-    setcount(count+1);
+ 
   }
   return <button onClick={handleclick} className="font-bold text-xl bg-white p-2 pl-4 pr-4 mt-5  rounded	border-b-2 border-t-2 border-r-2 border-l-2  border-black	hover:bg-zinc-800 hover:text-white hover:ease-in duration-150">Hire me {count} </button>
 }
